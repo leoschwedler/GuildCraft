0b6f05554771c64d88f2f88f79b4419e6c152b04
leonardo schwuedller <77402907+leoschwedler@users.noreply.github.com>
2025-09-24 18:23:57 -0300

    first commit

---
+++ src/main/java/GuildCraft/GuildCraftApplication.java
@@ -0,0 +1,13 @@
+package GuildCraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuildCraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuildCraftApplication.class, args);
+	}
+
+}
